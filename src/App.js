9a9af012d1d9cb3d5cda89e70145d02a9d26730b
aze <adam.gyulavari@lab.coop>
2017-07-06 22:40:59 +0200

    implement everything

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Websocket from 'react-websocket';
 import logo from './logo.svg';
 import './App.css';
 
+import Player from './Player.js'
+
 const mapObject = (object, callback) => {
   return Object.keys(object).map( key => callback(key, object[key]) )
 }
@@ -31,13 +33,15 @@ class App extends Component {
     super()
     this.state = {
       players: [],
-      25: [],
-      20: [],
-      19: [],
-      18: [],
-      17: [],
-      16: [],
-      15: []
+      allOut: {
+        25: [],
+        20: [],
+        19: [],
+        18: [],
+        17: [],
+        16: [],
+        15: []
+      }
     }
   }
   onMessage = (message) => {
@@ -47,20 +51,20 @@ class App extends Component {
     } else if (parsedMessage.command == 'insert_throw') {
       this.newThrow(parsedMessage.playerId, parsedMessage.throw)
     } else if (parsedMessage.command == 'edit_throw') {
-
+      window.location.reload()
     }
-    console.log(parsedMessage)
   }
   newThrow = (currentPlayer, t) => {
     this.setState({players: this.state.players.map((player) => {
       if (player.id == currentPlayer) {
         player[t.score] += t.modifier
         if (player[t.score] >= 3) {
-          if (!this.state[t.score].includes(player.id)) {
-            this.setState({[t.score]: [...this.state[t.score], player.id]})
+          if (!this.state.allOut[t.score].includes(player.id)) {
+            this.setState({allOut: Object.assign({}, this.state.allOut, {[t.score]: [...this.state.allOut[t.score], player.id]})})
+            //this.setState({[t.score]: [...this.state[t.score], player.id]})
           }
           if (player[t.score] > 3) {
-            if (this.state[t.score].length < this.state.players.length) {
+            if (this.state.allOut[t.score].length < this.state.players.length) {
               player.score += t.score * (player[t.score]-3)
             }
             player[t.score] = 3
@@ -101,28 +105,17 @@ class App extends Component {
         this.newThrow(round.player.id, t)
       })
     })
-    console.log(this.state)
-    // mapObject(game.players, (playerKey, player) => {
-    //   mapObject(player.rounds, (roundKey, round) => {
-    //     mapObject(round.throws, (throwKey, t) => {
-    //       if (t.score >= 15) {
-    //         player[t.score] += t.modifier
-    //       }
-    //     })
-    //   })
-    // })
+  }
+  filterAllOut() {
+    return Object.keys(this.state.allOut).filter(e => this.state.allOut[e].length === this.state.players.length).map(Number)
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="row title"><h1>Cricket Game</h1></div>
+        <div className="row">
+          { this.state.players.map((player) => <Player {...player} allOut={this.filterAllOut()} key={player.id} />) }
         </div>
-        { this.state.players.map((player) => <p>{player.name} - {player.score}</p>) }
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Websocket url='ws://localhost:8080/ws'
               onMessage={this.onMessage}/>
       </div>
---
+++ src/Player.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const keyList = [15, 16, 17, 18, 19, 20, 25]
+
+class Player extends Component {
+  getStyle(key) {
+    const {allOut, [key]: count} = this.props
+    const label = allOut.includes(key) ? 'danger' : count
+    return `label-${label}`
+  }
+  render() {
+    const {name, score} = this.props;
+    return (
+      <div className="col-md-6">
+        <div className="score">
+          <div className="header">
+            {name}
+          </div>
+          <div className="sum">
+            {score}
+          </div>
+          <div className="table-container">
+            {keyList.map((key) => {
+              return (<div className="table">
+                <ul>
+                  <li className="big">{key}</li>
+                  {[...Array(this.props[key]).keys()].map((k)=>(
+                    <li className={this.getStyle(key)}>
+                      <span>x</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>)
+            })}
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Player;
